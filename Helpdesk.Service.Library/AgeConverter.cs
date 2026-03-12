d240c1a42f273bbdfe2b08d6e3da42b14881a984
Artem Norenko <norenko.artem@gmail.com>
2026-03-12 15:36:01 +0100

    added custom object property mapping attributes

---
+++ Helpdesk.Service.Library/AgeConverter.cs
@@ -0,0 +1,8 @@
+﻿namespace Helpdesk.Service.Library
+{
+    public class AgeConverter
+    {
+        public int Convert(DateTime dateTime) =>
+            DateTime.Now.Year - dateTime.Year;
+    }
+}
---
+++ Helpdesk.Service.Library/ITypeConverter.cs
@@ -0,0 +1,13 @@
+﻿namespace Helpdesk.Service.Library
+{
+    public interface ITypeConverter<TSource, TTarget>
+        where TSource : notnull
+    {
+        TTarget Convert(TSource source);
+    }
+
+    public interface ITypeConverter
+    {
+        int Convert(DateTime source);
+    }
+}
